e455f20d79771d2cd8dbbcbc65d9902d602c23ae
felipeKPS2 <152972815+felipeKPS2@users.noreply.github.com>
2024-08-05 09:13:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto=29;
-prompt('Escolha umnúmero de 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+// se chute for igual ao número secreto
+if(chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o número secreto (29)');
+
+} else {
+    alert ('Você errou :-(')
+}
